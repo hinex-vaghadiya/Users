998ab66722278c820843fc4d907189030cb93448
hinex-vaghadiya <148621260+hinex-vaghadiya@users.noreply.github.com>
2025-12-24 08:56:50 +0530

    Update settings.py
    updated json only

--- core/settings.py
+++ core/settings.py
@@ -138,6 +138,9 @@ REST_FRAMEWORK = {
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+     'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
     
 
 }
