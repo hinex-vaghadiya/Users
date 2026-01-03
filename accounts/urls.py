c6998925044ce138886a4748279cb26cecb98117
hinex-vaghadiya <148621260+hinex-vaghadiya@users.noreply.github.com>
2026-01-03 17:11:44 +0530

    Update urls.py added token refresh api

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path,include
 from accounts.views import RegisterView,LoginView,profileView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns =[
     path('register',RegisterView.as_view()),
     path('login',LoginView.as_view()),
     path('account',profileView.as_view()),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
