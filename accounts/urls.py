e3f66462305566de5d949297dcf0104da52df135
hinex-vaghadiya <148621260+hinex-vaghadiya@users.noreply.github.com>
2026-01-05 12:53:05 +0530

    Update urls.py removed swagger docs

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+# from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns =[
     path('register',RegisterView.as_view()),
@@ -12,10 +12,10 @@ urlpatterns =[
     path('account',profileView.as_view()),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout',LogoutView.as_view()),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
+    # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # # Optional UI:
+    # path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
     
     
 
