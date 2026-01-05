8298e57c601ed7239251eaa65e3312bc948ddba2
hinex-vaghadiya <148621260+hinex-vaghadiya@users.noreply.github.com>
2026-01-05 12:27:13 +0530

    Update urls.py added swagger api docs

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,19 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 urlpatterns =[
     path('register',RegisterView.as_view()),
     path('login',LoginView.as_view()),
     path('account',profileView.as_view()),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout',LogoutView.as_view()),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
+    
     
 
 ]
